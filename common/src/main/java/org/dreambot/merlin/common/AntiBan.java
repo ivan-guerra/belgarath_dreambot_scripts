bdfd7e65f195a4dc11164a691d31e4836f8eabd3
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2026-06-21 12:37:31 -0400

    refactor: add javadoc and extract goAfk method
    Add .gitignore for Maven and jdtls-generated files and remove
    previously tracked build artifacts from target/ directories.
    
    Refactor AntiBan.run() to delegate to a new private goAfk(int, int)
    method, add Javadoc to class and public members, and log a message
    when returning from AFK.

--- common/src/main/java/org/dreambot/merlin/common/AntiBan.java
+++ common/src/main/java/org/dreambot/merlin/common/AntiBan.java
@@ -4,19 +4,41 @@ import org.dreambot.api.methods.Calculations;
 import org.dreambot.api.script.AbstractScript;
 import org.dreambot.api.utilities.Logger;
 
+/**
+ * Collection of anti-ban methods to make the bot less detectable.
+ */
 public class AntiBan {
-    private final AbstractScript script;
+  private final AbstractScript script;
 
-    public AntiBan(AbstractScript script) {
-        this.script = script;
-    }
+  /**
+   * @param script the running script used to invoke sleep
+   */
+  public AntiBan(AbstractScript script) {
+    this.script = script;
+  }
 
-    public void run() {
-        int roll = Calculations.random(0, 100);
-        if (roll < 2) {
-            int period_ms = Calculations.random(120000, 180000);
-            Logger.log("Anti-ban: Going AFK for " + period_ms / 1000 + "sec");
-            script.sleep(period_ms);
-        }
+  /**
+   * Rolls with a 2% chance to go AFK for a random period between 2-3 minutes.
+   */
+  public void run() {
+    int roll = Calculations.random(0, 100);
+
+    if (roll < 2) {
+      goAfk(2, 3);
     }
+  }
+
+  /**
+   * Simulates going AFK by sleeping for a random period of time.
+   *
+   * @param min_min minimum minutes to sleep
+   * @param max_min maximum minutes to sleep
+   */
+  private void goAfk(int min_min, int max_min) {
+    int period_ms = Calculations.random(min_min * 60000, max_min * 60000);
+
+    Logger.log("Anti-ban: Going AFK for " + period_ms / 1000 + "sec");
+    script.sleep(period_ms);
+    Logger.log("Anti-ban: Back from AFK");
+  }
 }
